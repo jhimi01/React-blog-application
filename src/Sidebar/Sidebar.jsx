618c2db5dab379c98d43dd7e79985e8fb1a69b30
Jhimi Talukder <talukderjhimi@gmail.com>
2023-04-27 13:00:45 +0600

    sidebar

--- src/Sidebar/Sidebar.jsx
+++ src/Sidebar/Sidebar.jsx
@@ -1,13 +1,40 @@
-import React from 'react';
-import './Sidebar.css'
-
+import React from "react";
+import "./Sidebar.css";
 
 const Sidebar = () => {
-    return (
-        <div className='sidebar'>
-            <h2>this is sidebar</h2>
+  return (
+    <div className="sidebar">
+      <div className="sidebarItem">
+        <span className="sidebarTitle">About ME</span>
+        <img
+          className="sidebarImg"
+          src="https://static.vecteezy.com/system/resources/previews/002/098/796/original/welcome-to-bangladesh-postcard-beautiful-landscape-view-with-famous-building-landmark-travel-and-safari-concept-of-asia-around-the-world-illustration-vector.jpg"
+          alt=""
+        />
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+      </div>
+      <div className="sidebarItem">
+        <span className="sidebarTitle">Category</span>
+        <ul className="sidebarList">
+          <li className="sidebarListItem">Life</li>
+          <li className="sidebarListItem">Music</li>
+          <li className="sidebarListItem">Style</li>
+          <li className="sidebarListItem">Sport</li>
+          <li className="sidebarListItem">Tech</li>
+          <li className="sidebarListItem">Cinema</li>
+        </ul>
+      </div>
+      <div className="sidebarItem">
+        <span className="sidebarTitle">Follow Us</span>
+        <div className="sidebarSocial">
+          <i className="sidebarIcon fa-brands fa-square-facebook"></i>
+          <i className="sidebarIcon fa-brands fa-square-twitter"></i>
+          <i className="sidebarIcon fa-brands fa-square-instagram"></i>
+          <i className="sidebarIcon fa-brands fa-square-pinterest"></i>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
